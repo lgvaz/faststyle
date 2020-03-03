5812ff070b72414c929cc52e5bef06ad3a2aa7d9
lgvaz <lucasgouvaz@gmail.com>
2020-03-03 18:17:57 -0300

    callbacks working

--- faststyle/layer_features.py
+++ faststyle/layer_features.py
@@ -23,30 +23,33 @@ def _get_layers(m, idxs):
   return [m[i] for i in idxs]
 
 # Cell
+_imagenet_norm = Normalize.from_stats(*imagenet_stats)
+
+# Cell
 class FeatModels:
   @staticmethod
   def vgg16():
     m = vgg16(True).features
     stl_ls = _get_layers(m, (1, 11, 18, 25))
     cnt_ls = _get_layers(m, (20,))
-    return dict(m=m, stl_ls=stl_ls, cnt_ls=cnt_ls)
+    return dict(m=m, stl_ls=stl_ls, cnt_ls=cnt_ls, tfms=[_imagenet_norm])
 
   @staticmethod
   def vgg19():
     m = vgg19(True).features
     stl_ls = _get_layers(m, (1, 6, 11, 20, 29))
     cnt_ls = _get_layers(m, (22,))
-    return dict(m=m, stl_ls=stl_ls, cnt_ls=cnt_ls)
+    return dict(m=m, stl_ls=stl_ls, cnt_ls=cnt_ls, tfms=[_imagenet_norm])
 
 # Cell
 class LayerFeats:
-  def __init__(self, m, stl_ls, cnt_ls):
-    self.m = _prepare_model(m)
+  def __init__(self, m, stl_ls, cnt_ls, tfms=None):
+    self.m, self.tfms = _prepare_model(m), Pipeline(tfms)
     self.stl_hooks = hook_outputs(stl_ls, detach=False)
     self.cnt_hooks = hook_outputs(cnt_ls, detach=False)
 
   def __call__(self, x):
-    _ = self.m(x)
+    _ = self.m(self.tfms(x))
     return Features(style=self.stl_hooks.stored, content=self.cnt_hooks.stored)
 
   @classmethod
