1d3b18718d764d0f49ece530c002a6d5d66ea5af
lgvaz <lucasgouvaz@gmail.com>
2020-03-29 15:32:01 -0300

    cleans code

--- faststyle/data.py
+++ faststyle/data.py
@@ -58,11 +58,9 @@ class Weights(TensorBase, ShowTitle):
 
 # Cell
 class ImageWeight(Tuple):
-  def show(self, ctx=None, **kwargs):
-    return show_titled_image(self, ctx=ctx, **kwargs)
-
   @classmethod
   def create(cls, fn, ws=None, imcls=PILImageX): return cls(imcls.create(fn), Weights(ifnone(ws, [1.])))
+  def show(self, ctx=None, **kwargs): return show_titled_image(self, ctx=ctx, **kwargs)
 ImageWeight.im, ImageWeight.ws = add_props(lambda i,x: x[i])
 
 # Cell
